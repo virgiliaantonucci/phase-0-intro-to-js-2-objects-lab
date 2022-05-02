de69ccd7912f95bcc780023135ea8b7ddf6ead42
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-05-01 22:19:18 -0400

    commit tree 57c5dde5256cdbb4f0259b4423a2b6c6cf40c200

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+/* const employee = {
+    name: 'sara',
+    streetAddress: '70th'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee}
+        newEmployee[key] = value
+        //newEmployee[key] allows me to access its value
+    return newEmployee 
+} */
+let employee = {
+    name: "Anna",
+    streetAddress: "700 Madison Avenue"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
